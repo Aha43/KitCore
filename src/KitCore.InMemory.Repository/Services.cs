d005c0e11b0832245265a4d76d4c2bb986e2df5f
Arne Halvorsen <arne.halvorsen@gmail.com>
2025-02-08 12:02:28 +0100

    added IoC for the in memory repositories

---
+++ src/KitCore.InMemory.Repository/Services.cs
@@ -0,0 +1,31 @@
+using Microsoft.Extensions.DependencyInjection;
+using KitCore.Domain.Repository;
+
+namespace KitCore.InMemory.Repository;
+
+public static class Services
+{
+    public static IServiceCollection AddKitCoreInMemoryRepositories(this IServiceCollection services)
+    {
+        services.AddSingleton<ISystemRepository, InMemorySystemRepository>();
+        services.AddSingleton<ISystemPartRepository, InMemorySystemPartRepository>();
+        services.AddSingleton<IPurchaseRepository, InMemoryPurchaseRepository>();
+        services.AddSingleton<IUseCaseRepository, InMemoryUseCaseRepository>();
+        services.AddSingleton<IUseCaseStepRepository, InMemoryUseCaseStepRepository>();
+        services.AddSingleton<IUseCaseStepAssignmentRepository, InMemoryUseCaseStepAssignmentRepository>();
+        services.AddSingleton<ISystemAttributeRepository, InMemorySystemAttributeRepository>();
+        services.AddSingleton<ISystemPartAttributeRepository, InMemorySystemPartAttributeRepository>();
+        services.AddSingleton<IAttributeTypeRepository, InMemoryAttributeTypeRepository>();
+        services.AddSingleton<IAttributeValueRepository, InMemoryAttributeValueRepository>();
+        services.AddSingleton<ISystemTypeRepository, InMemorySystemTypeRepository>();
+        services.AddSingleton<ISystemPartTypeRepository, InMemorySystemPartTypeRepository>();
+        services.AddSingleton<ISystemTypeAttributeTypeRepository, InMemorySystemTypeAttributeTypeRepository>();
+        services.AddSingleton<ISystemPartTypeAttributeTypeRepository, InMemorySystemPartTypeAttributeTypeRepository>();
+        services.AddSingleton<IBrandSetRepository, InMemoryBrandSetRepository>();
+        services.AddSingleton<ICurrencySetRepository, InMemoryCurrencySetRepository>();
+        services.AddSingleton<IModelSetRepository, InMemoryModelSetRepository>();
+        services.AddSingleton<IDataTypeSetRepository, InMemoryDataTypeSetRepository>();
+        services.AddSingleton<IUnitSetRepository, InMemoryUnitSetRepository>();
+        return services;
+    }
+}
